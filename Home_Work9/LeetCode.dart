909a02d30979b62ea116a687c175bfac2eb0ffee
Abghfar123 <mahmoudabdelghfar71@gmail.com>
2026-02-17 01:00:45 +0200

    Home_Work9

---
+++ Home_Work9/LeetCode.dart
@@ -0,0 +1,11 @@
+class Solution {
+  int singleNumber(List<int> nums) {
+    int result = 0;
+
+    for (int i = 0; i < nums.length; i++) {
+      result = result ^ nums[i];
+    }
+
+    return result;
+  }
+}
